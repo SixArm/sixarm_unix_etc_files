69f822cd179f900d7a8afe7f578a3126a9ed2241
Joel Parker Henderson <joel@joelparkerhenderson.com>
2014-12-15 11:34:31 -0700

    Refactor environment to its own repo

--- environment.sh
+++
@@ -1,2 +0,0 @@
-# Create the `environment` file by concatenating the `environment.d` files.
-awk 'FNR==1 && NR!=1 {print "\n##########################################################################\n"}{print}' environment.d/*.txt > environment
